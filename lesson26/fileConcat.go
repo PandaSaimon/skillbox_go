cc13ed837d08c837f5bd2bce0ea91e3a3b501d00
Vasily Simonov <vsimonov3@yandex.ru>
2023-01-23 23:48:51 +0300

    lesson26 logging delete

--- lesson26/fileConcat.go
+++ lesson26/fileConcat.go
@@ -3,50 +3,56 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
 	"strings"
 )
 
 const defName = "default name"
 
-func readFile(fileName string) string {
+func readFile(fileName string) (string, error) {
 	file, err := os.Open(fileName)
 	if err != nil {
-		log.Fatalln(err)
-		return ""
+		return "", err
 	}
 	defer file.Close()
 	fileBody2, err := ioutil.ReadAll(file)
 	if err != nil {
-		log.Fatalln(err)
-		return ""
+		return "", err
 	}
-	return string(fileBody2)
+	return string(fileBody2), nil
 }
 
 func main() {
 	args := os.Args[1:]
 	result := ""
 	if len(args) <= 0 {
-		log.Fatalln("No data input")
+		fmt.Println("No data input")
 		return
 	}
-	result = readFile(args[0])
+	result, err := readFile(args[0])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	if len(args) > 1 {
-		result = strings.Join([]string{result, readFile(args[1])}, "\n")
+		result2, err := readFile(args[1])
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		result = strings.Join([]string{result, result2}, "\n")
 
 		if len(args) > 2 {
 			fileResultName := args[2]
 			file, err := os.Create(fileResultName)
 			if err != nil {
-				log.Fatalln(err)
+				fmt.Println(err.Error())
 				return
 			}
 			defer file.Close()
 			file.WriteString(result)
-			log.Println("Результат сохранен в", fileResultName)
+			fmt.Println("Результат сохранен в", fileResultName)
 		}
 	}
 
